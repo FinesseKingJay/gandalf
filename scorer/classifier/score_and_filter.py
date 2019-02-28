84000582de50c91562cd68a85250df1a6fde8aa2
Kostiantyn Omelianchuk <komelianchuk@gmail.com>
2019-02-28 15:17:49 +0200

    Finsih score and filter script

--- scorer/classifier/score_and_filter.py
+++ scorer/classifier/score_and_filter.py
@@ -7,7 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 from grampy.api import opc_check
 from grampy.text import AnnotatedText, AnnotatedTokens
 
-from scorer.helpers.utils import read_lines
+from scorer.helpers.utils import read_lines, write_lines
 from scorer.classifier.get_features import get_protected_response
 
 
@@ -34,8 +34,10 @@ def main(args):
     # run sentences through OPC if it needed
     if args.opc:
         with ThreadPoolExecutor(args.n_threads) as pool:
-            out_opc = pool.map(wrap_opc, sentences)
-        opc_out = [x for x in out_opc]
+            opc_out = pool.map(wrap_opc, sentences)
+        opc_out = [x for x in opc_out]
+        out_file = args.output_file.replace(".txt", f"_opc.txt")
+        write_lines(out_file, opc_out)
     else:
         opc_out = sentences
 
@@ -44,16 +46,22 @@ def main(args):
     with ThreadPoolExecutor(args.n_treads) as pool:
         scorer_out = pool.map(wrap_confidence_scorer, combined)
     scorer_out = [x for x in scorer_out]
-
+    out_file = args.output_file.replace(".txt", f"_scored.txt")
+    write_lines(out_file, scorer_out)
+    
     # apply thresholds
-    t = args.threshold
-    for sent in scorer_out:
-        ann_sent = AnnotatedTokens(AnnotatedText(sent))
-        for ann in ann_sent.iter_annotations():
-            score = ann.meta['confidence']
-            if score < t:
-                ann_sent.remove(ann)
-
+    thresholds = [0, 0.3, 0.5, 0.7]
+    for t in thresholds:
+        t_out = []
+        for sent in scorer_out:
+            ann_sent = AnnotatedTokens(AnnotatedText(sent))
+            for ann in ann_sent.iter_annotations():
+                score = ann.meta['confidence']
+                if score < t:
+                    ann_sent.remove(ann)
+            t_out.append(ann_sent.get_annotated_text())
+        out_file = args.output_file.replace(".txt", f"_above_{t}.txt")
+        write_lines(out_file, t_out)
 
 
 
@@ -62,6 +70,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file',
                         help='Path to the input file',
+                        )
+    parser.add_argument('output_file',
+                        help='Path to the output file',
                         )
     parser.add_argument('--server_path',
                         help='Path to the server',
